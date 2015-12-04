d01fd9f5787c3e4df538afaf43ddc19159096f9c
Gary Pang <gary.pang@gmail.com>
2015-12-04 03:26:02 -0500

    Add user auth
    Create service, controller and template page for authenticating user.

---
+++ components/auth/authService.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular
+      .module('statusApp')
+      .factory('Auth', AuthService);
+
+    function AuthService($firebaseAuth) {
+        var ref = new Firebase("https://statusapp.firebaseio.com");
+        return $firebaseAuth(ref);
+    }
+
+})();
